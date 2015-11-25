dceb4d67df0b518950b4024eb3113be63b44d61c
tooleks <tooleks@gmail.com>
2015-11-25 19:17:37 +0200

    updated ui message

--- views/questionnaire/update.php
+++ views/questionnaire/update.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 
 $this->title = Yii::t('app', 'Update {modelClass}: ', [
     'modelClass' => 'Questionnaire',
-]) . ' ' . $model->id_ank;
+]) . ' ' . $model->name_ua;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Questionnaires'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id_ank, 'url' => ['view', 'id' => $model->id_ank]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
